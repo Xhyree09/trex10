dad637a738d0eec0947a70cab227ec5e53079dfb
Xhyree09 <84895683+Xhyree09@users.noreply.github.com>
2021-09-30 16:25:42 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var trex, trexAnimation;
+
+function preload(){
+trexAnimation = loadAnimation("")
+
+}
+function setup() 
+{
+  createCanvas(600,200);
+}
+
+function draw() 
+{
+background("purple");
+
+}
+
